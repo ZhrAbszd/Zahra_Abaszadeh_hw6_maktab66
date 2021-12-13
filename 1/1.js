922edb9378872843cc7a36fdea595563adfeebe5
Zahra <abbaszadeh.zhr@gmail.com>
2021-12-13 17:41:29 +0330

    Third function Q1

--- 1/1.js
+++ 1/1.js
@@ -1,18 +1,18 @@
 //First => with split & indexOf & splice :
 function Count(str1, str2) {
-    let array1 = str1.split('');
-    let array2 = str2.split('');
-    let counter = 0;
+    let array1 = str1.split('')
+    let array2 = str2.split('')
+    let counter = 0
     for (let i in array1) {
-        let index = array2.indexOf(array1[i]);
+        let index = array2.indexOf(array1[i])
         if (index !== -1) {
-            array2.splice(index, 1);
+            array2.splice(index, 1)
             counter  ++
         }
     }
     return counter 
 }
-console.log(Count("aabcc", "adcaa"));
+console.log(Count("aabcc", "adcaa"))
 
 //************************************************//
 
@@ -22,18 +22,35 @@ function commonCount(str1,str2){
     while(str1.length & str2.length){
         if(str2.includes(str1.charAt(0))){
             counter++
-            str2 = str2.replace(str1.charAt(0),"");
-            str1 = str1.slice(1);
+            str2 = str2.replace(str1.charAt(0),"")
+            str1 = str1.slice(1)
         }
         else {
-            str1 = str1.slice(1);
+            str1 = str1.slice(1)
         }
     }
-    return counter;
+    return counter
 }
-console.log(commonCount("aabcc","adcaa"));
+console.log(commonCount("aabcc","adcaa"))
 
 //************************************************//
+
+
+// Third => with split & findIndex & splice
+function SameCount(str1, str2) {
+    let counter = 0
+    let temp = str2.split("")
+    for(let str of str1){
+      let index = temp.findIndex(s => s === str)
+      if(index >= 0){
+        counter++;
+        temp.splice(index, 1)
+      }
+    }
+    return counter
+  }
+  
+console.log(SameCount("aabcc", "adcaa"))
 
 
 
